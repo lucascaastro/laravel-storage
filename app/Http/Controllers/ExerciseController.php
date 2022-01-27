dc9d5aa9b5c2063d94907761de8c5dac9d60d085
lucascaastro <lucasgrrock@gmail.com>
2022-01-26 21:53:46 -0300

    first commit

---
+++ app/Http/Controllers/ExerciseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExerciseController extends Controller
+{
+    //
+
+    public function photo(Request $request)
+    {
+        $request->photo->storeAs('public', 'file.jpg');
+
+        return 'Deu certo';
+    }
+}
