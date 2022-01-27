56aee3e1e76cf446ba3ccfc5f4b787f86aba3210
lucascaastro <lucasgrrock@gmail.com>
2022-01-27 16:51:07 -0300

    images

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -10,8 +10,8 @@ class ExerciseController extends Controller
 
     public function photo(Request $request)
     {
-        $request->image1->storeAs('public', 'file1.jpg');
-        $request->image2->storeAs('public', 'file2.jpg');
+        $request->image1->storeAs('public/images', 'file1.jpg');
+        $request->image2->storeAs('public/images', 'file2.jpg');
 
         return 'Deu certo';
     }
